da3dcda1f467aaacd0df8a6cc4edf03785efb8af
Myo Myat Han <myomyathan.dev@gmail.com>
2024-07-03 19:26:11 +0630

    api first commit

---
+++ database/migrations/2024_07_03_074637_create_grades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('student_id')->unsigned();
+            $table->integer('subject_id')->unsigned();
+            $table->integer('grade');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
